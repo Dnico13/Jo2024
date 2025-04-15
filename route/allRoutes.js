2e6ffe1e605b5e9be78dff22da7db29dba642706
Dnico13 <Dnico13@hotmail.com>
2025-04-16 01:49:08 +0200

    front

--- route/allRoutes.js
+++ route/allRoutes.js
@@ -2,8 +2,12 @@ import Route from "./Route.js";
 
 //Définition de toutes les routes de l'application
 export const allRoutes = [
-    new Route("/", "Accueil des JO Paris 2024", "./Pages/home.html"),
-    new Route("/accueil", "Presentation des manisfestations des JO de Paris", "./Pages/accueil.html"),
+    new Route("/", "Bienvenue sur le site des JO Paris 2024", "./Pages/home.html"),
+    new Route("/accueil", "Presentation des manisfestations des JO de Paris", "Pages/accueil.html"),
+    new Route("/mentions", "Presentation des mentions légales du site", "Pages/mentions.html"),
+    new Route("/presentationOffres", "Presentation des Offres", "Pages/presentationOffres.html"),
+    new Route("/login", "Page de Login", "Pages/login.html"),
+    new Route("/panier", "Visualiser son panier", "Pages/panier.html"),
    
     
 
